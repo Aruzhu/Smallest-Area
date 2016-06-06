1273bc51a761bf88dd5c1a3d4842d90370c94e5d
Jon Elias <jon39334@gmail.com>
2016-06-06 21:11:03 +0200

    the handling could have been done better?

--- main.cpp
+++ main.cpp
@@ -33,16 +33,21 @@ int main(){
 		int indexes[2] = {input.find_first_of('*'), input.find(')',0)}
 		indexes[2] = input.find('*', indexes[0]);
 		
+		string numb;
 		for(int i = 0; i<2; i++){
 			findtest(indexes[i]);
+			switch(i){
+				case 0 : 
+					int width = stringint( numb.assign(input, 1, index-1) );
+					break;
+				case 1 :
+					int length = stringint( numb.assign(input, indexes[0], indexes[1] ) );
+					break;
+				case 2 :
+					int count = stringint( numb.assign(input, indexes[2], input.size());
+					break;
+			}
 		}
-		findtest(index);
-		findtest(index2);
-		
-		string numb;
-		numb.assign(input, 1, index-1);
-		int width = stringint(numb.assign(input, 1, index-1));
-		
 	}
 	
 }
